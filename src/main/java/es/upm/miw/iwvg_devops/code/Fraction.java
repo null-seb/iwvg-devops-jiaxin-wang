0adc1b8aac2bc74248a4bd3aa5e5a3ed60274bae
Sebastian <wangjiaxin6667@gmail.com>
2020-10-04 15:16:55 +0200

    Fraction&FractionTest Ampliation #9

--- src/main/java/es/upm/miw/iwvg_devops/code/Fraction.java
+++ src/main/java/es/upm/miw/iwvg_devops/code/Fraction.java
@@ -59,6 +59,61 @@ public class Fraction {
         return (double) numerator / denominator;
     }
 
+    public boolean isProper(int numerator,int denominator){
+        if (denominator>numerator){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public boolean isImproper(int numerator,int denominator){
+        if (numerator>denominator){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public  boolean isEquivalent(int numerator,int denominator){
+        if (numerator==denominator){
+            return true;
+        }else {
+            return false;
+        }
+
+    }
+
+    public Fraction add(Fraction summand){
+        if (this.denominator%summand.getDenominator()==0) {
+            summand.setNumerator(summand.getNumerator() * (this.denominator / summand.getDenominator()));
+            summand.setDenominator(this.denominator);
+        }else if(summand.getDenominator()%this.denominator==0){
+            this.setNumerator(summand.numerator * (summand.getDenominator() / this.denominator));
+            this.setDenominator(summand.denominator);
+        }
+        else{
+            int tmp = this.denominator;
+
+            this.setDenominator(this.denominator * summand.getDenominator());
+            this.setNumerator(this.numerator * summand.getDenominator());
+
+            summand.setDenominator(tmp * summand.getDenominator());
+            summand.setNumerator(tmp * summand.getNumerator());
+        }
+        return new Fraction(this.numerator + summand.getNumerator(), this.denominator);
+    }
+
+    public Fraction multiply(Fraction multiplier){
+        Fraction temp=new Fraction((this.numerator*multiplier.numerator),(this.denominator*multiplier.denominator));
+        return temp;
+    }
+
+    public Fraction divide(Fraction divisor){
+        Fraction temp=new Fraction((this.numerator*divisor.denominator),(this.denominator*divisor.numerator));
+        return temp;
+    }
     @Override
     public String toString() {
         return "Fraction{" +
--- src/test/java/es/upm/miw/iwvg_devops/rest/FractionTest.java
+++ src/test/java/es/upm/miw/iwvg_devops/rest/FractionTest.java
@@ -24,7 +24,7 @@ public class FractionTest {
         assertEquals(1, fraction.getNumerator());
     }
     @Test
-    void testFractionsetSetDenominator() {
+    void testFractionSetDenominator() {
         fraction.setDenominator(2);
         assertEquals(2, fraction.getDenominator());
     }
@@ -37,9 +37,43 @@ public class FractionTest {
         assertEquals(1, fraction.getDenominator());
     }
     @Test
-    void testFractiondecimal(){
+    void testFractionDecimal(){
         assertEquals(0, fraction.decimal());
     }
-
-
+    @Test
+    void testIsProper(){
+        assertEquals(true,fraction.isProper(1,2));
+    }
+    @Test
+    void testISImproper(){
+        assertEquals(false,fraction.isImproper(1,2));
+    }
+    @Test
+    void testIsEquivalent(){
+        assertEquals(true,fraction.isEquivalent(1,1));
+    }
+    @Test
+    void testAdd(){
+        Fraction fractionTemp;
+        Fraction fractionSum ;
+        fractionTemp=new Fraction(1,3);
+        fractionSum=new Fraction(0,1);
+        assertEquals(fractionSum.toString(),fraction.add(fraction).toString());
+    }
+    @Test
+    void testMultiply(){
+        Fraction fractionTemp;
+        Fraction fractionResult ;
+        fractionTemp=new Fraction(3,5);
+        fractionResult=new Fraction(0,5);
+        assertEquals(fractionResult.toString(),fraction.multiply(fractionTemp).toString());
+    }
+    @Test
+    void testDivide(){
+        Fraction fractionTemp;
+        Fraction fractionResult ;
+        fractionTemp=new Fraction(2,3);
+        fractionResult=new Fraction(0,2);
+        assertEquals(fractionResult.toString(),fraction.divide(fractionTemp).toString());
+    }
 }
